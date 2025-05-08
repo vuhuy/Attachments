2857ec201da85b189ae0844a4a31a8862e4d3f11
vuhuy <vuhuy.luu@outlook.com>
2025-05-08 22:49:23 +0200

    Add basic tests with selenium

---
+++ tests/scripts/prepare.sh
@@ -0,0 +1,33 @@
+php maintenance/run.php install.php \
+    --dbname=mediawiki \
+    --dbserver=database \
+    --installdbuser=mediawiki \
+    --installdbpass=mediawiki1234 \
+    --dbuser=mediawiki \
+    --dbpass=mediawiki1234 \
+    --server='http://localhost:8080' \
+    --scriptpath='' \
+    --lang=en \
+    --pass=mediawiki1234 \
+    'Attapedia' 'Attachment' &&
+sed -i "1a\\
+error_reporting( -1 );\\
+ini_set( 'display_errors', 1 );\\
+\$wgShowExceptionDetails = true;\\
+\$wgShowDBErrorBacktrace = true;
+" LocalSettings.php &&
+echo "wfLoadExtension( 'WikiEditor' );" >> LocalSettings.php  &&
+echo "wfLoadExtension( 'CodeEditor' );" >> LocalSettings.php  &&
+echo "wfLoadExtension( 'VisualEditor' );" >> LocalSettings.php  &&
+echo "wfLoadExtension( 'Attachments' );" >> LocalSettings.php  &&
+echo "\$wgAttachmentsShowInNamespaces = true;" >> LocalSettings.php &&
+echo "\$wgAttachmentsShowInViews = true;" >> LocalSettings.php &&
+echo "\$wgAttachmentsShowEmptySection = true;" >> LocalSettings.php &&
+echo "\$wgNamespacesWithSubpages[NS_MAIN] = 1;" >> LocalSettings.php &&
+echo "\$wgEnableUploads = true;" >> LocalSettings.php &&
+echo "\$wgGroupPermissions['*']['upload'] = true;" >> LocalSettings.php &&
+echo "\$wgGroupPermissions['*']['reupload'] = true;" >> LocalSettings.php &&
+echo "\$wgRateLimits = [];" >> LocalSettings.php &&
+apachectl graceful &&
+php maintenance/run.php edit.php --conf LocalSettings.php Control < extensions/Attachments/tests/data/Control.html
+php maintenance/run.php edit.php --conf LocalSettings.php Attachments < extensions/Attachments/tests/data/Control.html
