6c47e613805f44d704c369ecf030af79cf2adb2c
vuhuy <vuhuy.luu@outlook.com>
2025-05-13 00:53:41 +0200

    Backport Minerva Neue fix

--- modules/ext.attachments.minerva-workaround.js
+++ modules/ext.attachments.minerva-workaround.js
@@ -1,7 +1,7 @@
 mw.loader.using('mediawiki.util', function() {
     const data = mw.config.get( 'attachmentsMinervaWorkaround' );
     if (data && data.useWorkaround) {
-        const pViews = document.querySelector("#p-views");
+        const pViews = document.querySelector("#page-actions");
         if (pViews) {
             const children = pViews.children;
             const before = children.length > 1 ? children[children.length - 1] : null;
@@ -10,12 +10,9 @@ mw.loader.using('mediawiki.util', function() {
                 attachment.className = "page-actions-menu__list-item";
                 attachment.innerHTML = `
                     <a role="button" href="${data.href}" data-mw="interface" title="${data.text}" 
-                       class="cdx-button cdx-button--size-large cdx-button--fake-button 
-                              cdx-button--fake-button--enabled cdx-button--icon-only 
-                              cdx-button--weight-quiet">
-                        <span class="mw-ui-icon-minerva-attach" 
-                              style="min-width:20px;min-height:20px;width:1.25rem;height:1.25rem;display:inline-block;background-size:cover;background-position:center;"></span>
-                        <span>${data.text}</span>
+                       class="menu__item--page-actions-history mw-ui-icon mw-ui-icon-element
+                       mw-ui-icon-minerva-attach mw-ui-icon-with-label-desktop mw-ui-button mw-ui-quiet">
+                       ${data.text}
                     </a>
                 `;
                 pViews.insertBefore(attachment, before);
