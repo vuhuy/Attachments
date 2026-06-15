f1cd895f8d75ad415567af7d1a44874acc8cb1db
vuhuy <vuhuy.luu@outlook.com>
2026-06-16 00:50:09 +0200

    Fix deprecation dynamic properties

--- includes/AttachAction.php
+++ includes/AttachAction.php
@@ -12,6 +12,9 @@ class AttachAction extends Action {
 }
 
 class SpecialAttach extends SpecialUpload {
+	public $prefix = '';
+	public $title = '';
+
 	function execute( $title ) {
 		$req = $this->getRequest();
 
